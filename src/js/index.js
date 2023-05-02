6688e98e0aaa020dc462f53bec98e1ec2e36d9ca
Aya Iida <ayaomi0811@gmail.com>
2023-05-02 14:06:04 -0700

    modified sitterlist html css

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,19 @@
+//Humberger menu
+const open = document.getElementById("open");
+const overlay = document.querySelector(".overlay");
+const close = document.getElementById("close");
 
+open.addEventListener("click",()=>{
+    overlay.classList.add("show");
+    open.classList.add("hide");
+});
+
+close.addEventListener("click",()=>{
+    overlay.classList.remove("show");
+    open.classList.remove("hide");
+});
+
+//Q&A section
 const h4s = document.querySelectorAll("h4");
 h4s.forEach(h4=>{
     h4.addEventListener("click",()=>{
