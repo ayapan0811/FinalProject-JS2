77f6b3b186eafe7d9bb8d56b840e3de7e2bd63f0
Aya Iida <ayaomi0811@gmail.com>
2023-05-07 16:39:27 -0700

    modified Header of tablet desktop

--- src/component/signIn/signIn.js
+++ src/component/signIn/signIn.js
@@ -2,21 +2,23 @@
 function SignInPage(){
     return(
         <main class="singUp">
-            <form action="#">
-                <h1>Sign In to Dog Sitter </h1>
-                <aside>
-                    <label htmlFor="email">Email</label>
-                    <input type="text" name="email" id="email"placeholder="Your Email"/>
-                </aside>
-                <aside>
-                    <label htmlFor="password">Password</label>
-                    <input type="password" name="password" id="password"placeholder="Password"/>
-                </aside>
-                <aside>
-                    <button>Sign In</button>
-                    <p>Don't have an account <a href="/signUp.html">Create here</a></p>
-                </aside>
-            </form>
+            <section>
+                <form action="#">
+                    <h1>Sign In to Dog Sitter </h1>
+                    <aside>
+                        <label htmlFor="email">Email</label>
+                        <input type="text" name="email" id="email"placeholder="Your Email"/>
+                    </aside>
+                    <aside>
+                        <label htmlFor="password">Password</label>
+                        <input type="password" name="password" id="password"placeholder="Password"/>
+                    </aside>
+                    <aside>
+                        <button>Sign In</button>
+                        <p>Don't have an account <a href="/signUp.html">Create here</a></p>
+                    </aside>
+                </form>
+            </section>
     </main>
     );
 }
--- src/component/signUp/signUp.js
+++ src/component/signUp/signUp.js
@@ -2,36 +2,39 @@
 function SignUpPage(){
     return(
         <main className="singUp">
-            <form action="#">
-                <h1>Sing Up for Dog Sitter </h1>
-                <aside>
-                    <label htmlFor="fName">First Name</label>
-                    <input type="text" name="fName" id="fName"placeholder="Name"/>
-                </aside>
-                <aside>
-                    <label htmlFor="location">Location</label>
-                    <select name="location" id="location">
-                        <option value="null"></option>
-                        <option value="Vancouver">Vancouver</option>
-                        <option value="Montreal">Montreal</option>
-                        <option value="Toronto">Toronto</option>
-                    </select>
-                </aside>
-                <aside>
-                    <label htmlFor="email">Email</label>
-                    <input type="text" name="email" id="email"placeholder="Email"/>
-                </aside>
-                <aside>
-                    <label htmlFor="password">Password</label>
-                    <input type="password" name="password" id="password"placeholder="Password"/>
-                </aside>
-                    <label htmlFor="cnf-password">Confirm Password</label>
-                    <input type="password" name="cnf-password" id="cnf-password"
-                    placeholder="Confirm Password"/>
-                <aside className="sign-up">
-                    <button>Sing Up</button>
-                </aside>
-            </form>
+            <section>
+                <form action="#">
+                    <h1>Sing Up for Dog Sitter </h1>
+                    <aside>
+                        <label htmlFor="fName">First Name</label>
+                        <input type="text" required name="fName" id="fName"placeholder="Name"/>
+                    </aside>
+                    <aside>
+                        <label htmlFor="location">Location</label>
+                        <select name="location" id="location">
+                            <option value="null"></option>
+                            <option value="Vancouver">Vancouver</option>
+                            <option value="Montreal">Montreal</option>
+                            <option value="Toronto">Toronto</option>
+                        </select>
+                    </aside>
+                    <aside>
+                        <label htmlFor="email">Email</label>
+                        <input type="text" required name="email" id="email"placeholder="Email"/>
+                    </aside>
+                    <aside>
+                        <label htmlFor="password">Password</label>
+                        <input type="password" required name="password" id="password"placeholder="Password"/>
+                    </aside>
+                        <label htmlFor="cnf-password">Confirm Password</label>
+                        <input type="password" name="cnf-password" id="cnf-password"
+                        placeholder="Confirm Password"/>
+                        <p id="message">demo text</p>
+                    <aside className="sign-up">
+                        <button>Sing Up</button>
+                    </aside>
+                </form>
+            </section>
     </main>
     );
 }
@@ -45,6 +48,7 @@ function App(){
             let email = $("#email").val();
             let password = $("#password");
             let confirmPassword = $("#cnf-password");
+            let message = $("#message");
 
             // Check that all required fields are filled out
             if(name == "" || location == "" || email == "" || password == "" || confirmPassword == ""){
@@ -52,14 +56,21 @@ function App(){
                 return false;
             }
             // Check that the password and confirm password fields match
-            if(password != confirmPassword){
-                alert("The password and confirm password fields do not match");
-                return false;
+            // if(password != confirmPassword){
+            //     alert("The password and confirm password fields do not match");
+            //     return false;
+            // }
+
+            // Check that the password and confirm password
+            if($("#password") == $("#cnf-password")){
+                $("#message").text("Password match!");
+                $("#message").css("color","green");
+            }else{
+                $("#message").text("Password don't match!");
+                $("#message").css("color","red");
             }
         });
         return true;
-
-
     });
 
     return(
