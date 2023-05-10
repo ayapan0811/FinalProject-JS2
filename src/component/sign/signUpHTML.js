bfd7d82eef9adbfd7d7f5df6d974004315de2ff8
Katsushi Torii <kat352soccer1@gmail.com>
2023-05-10 00:28:30 -0700

    modified read me, bawwaw to bowwow

--- src/component/sign/signUpHTML.js
+++ src/component/sign/signUpHTML.js
@@ -90,7 +90,7 @@ function Footer(){
                     </ul>
                 </section>
             </section>
-            <p>Copyright &copy; Bawwow sitter service </p>
+            <p>Copyright &copy; Bowwow sitter service </p>
         </footer>
     );
 }
